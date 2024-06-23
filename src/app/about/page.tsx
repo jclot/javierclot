6f3eb97749636ff4843835df8bc428203cad1bde
JulianClotCordoba <jclot60815@ufide.ac.cr>
2024-06-22 21:33:53 -0600

    Project setup

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import React from "react";
+
+const About: React.FC = () => {
+  return (
+    <Link href="/">Home</Link>
+  );
+};
+
+export default About;
---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../Navbar/navbar.module.css";
+
+const Navbar: React.FC = () => {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <Link href="/">J. CLOT</Link>
+            </div>
+            <ul className={styles.navLinks}>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/areas">Areas of practice</Link>
+                </li>
+                <li>
+                    <Link href="/industries">Industries</Link>
+                </li>
+                <li>
+                    <Link href="/about">About us</Link>
+                </li>
+                <li>
+                    <Link href="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
