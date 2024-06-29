b6f0e44435b4cb1f37cc7c40acdeb926b7d154b9
JulianClotCordoba <jclot60815@ufide.ac.cr>
2024-06-29 02:58:36 -0600

    Call <ContactForm /> component

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+import ContactForm from "@/components/Contact/contact";
+
+const Contact: React.FC = () => {
+  return (
+    <ContactForm />
+  );
+};
+
+export default Contact;
