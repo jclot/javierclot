57c1d20b13621dfaa5c5d2634fcd40017ddfa311
JulianClotCordoba <jclot60815@ufide.ac.cr>
2024-07-12 16:15:00 -0600

    Create areas of practice page

---
+++ src/app/areas/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import React from "react";
+
+const Areas: React.FC = () => {
+  return (
+    <Link href="/">Areas of practice</Link>
+  );
+};
+
+export default Areas;
