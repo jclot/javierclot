1c8c96aab2cfbda7dc72ad794e379402f434f2fc
JulianClotCordoba <jclot60815@ufide.ac.cr>
2024-07-12 16:16:02 -0600

    Change to camelCase

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -3,35 +3,38 @@ import styles from "./footer.module.css";
 
 const Footer: React.FC = () => {
     return (
-        <footer className={styles.footer}>
-            <div className={styles.footer_content}>
-                <div className={styles.footer_section}>
-                    <h2>Contact me</h2>
-                    <p>WhatsApp: <a href="tel:+50683827865">(+506) 8382-7865</a></p>
-                    <p>Email: <a href="mailto:jsauma@saumarossi.com">jaclot@hotmail.com</a></p>
+        <div>
+            <hr className={styles.mainContentBreak} />
+            <footer className={styles.footer}>
+                <div className={styles.footerContent}>
+                    <div className={styles.footerSection}>
+                        <h2>Contact me</h2>
+                        <p>WhatsApp: <a href="tel:+50683827865">(+506) 8382-7865</a></p>
+                        <p>Email: <a href="mailto:jaclot@hotmail.com">jaclot@hotmail.com</a></p>
+                    </div>
+                    <div className={styles.footerLogo}>
+                        <h1>JAVIER CLOT</h1>
+                    </div>
+                    <div className={styles.footerSection}>
+                        <h2>Address</h2>
+                        <p>C087, San José, San Pedro, Freses, 11801, CR</p>
+                    </div>
+                    <div className={styles.footerSection}>
+                        <h2>Links</h2>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/areas">Areas of Practice</a></li>
+                            <li><a href="/industries">Industries</a></li>
+                            <li><a href="/about">About Us</a></li>
+                            <li><a href="/contact">Contact</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className={styles.footer_logo}>
-                    <h1>JAVIER CLOT</h1>
+                <div className={styles.footerBottom}>
+                    <p>&copy; {new Date().getFullYear()} JAVIER CLOT. All rights reserved. Created by <a href="https://github.com/jclot/">Julian Clot</a>.</p>
                 </div>
-                <div className={styles.footer_section}>
-                    <h2>Address</h2>
-                    <p>37 Street, Barrio Dent, San José 11501, CR</p>
-                </div>
-                <div className={styles.footer_section}>
-                    <h2>Links</h2>
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/areas">Areas of Practice</a></li>
-                        <li><a href="/industries">Industries</a></li>
-                        <li><a href="/about">About Us</a></li>
-                        <li><a href="/contact">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div className={styles.footer_bottom}>
-                <p>&copy; {new Date().getFullYear()} SAUMA ROSSI. All rights reserved. Powered by Neurobrand.</p>
-            </div>
-        </footer>
+            </footer>
+        </div>
     );
 };
 
