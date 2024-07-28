6dbfad5062109b235ae1775a6be3f216cc1bf627
JulianClotCordoba <jclot60815@ufide.ac.cr>
2024-07-28 00:56:12 -0600

    Change space layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "../styles/globals.css";
 import Navbar from "@/components/Navbar/navbar";
 import Footer from "@/components/Footer/footer";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
