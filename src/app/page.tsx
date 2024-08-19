3c2df64a39840d3381caa8f568ee2c5eebebd34d
JulianClotCordoba <jclot60815@ufide.ac.cr>
2024-08-19 14:52:23 -0600

    Complete the home-lobby biography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             <h1>JAVIER CLOT</h1>
           </div>
           <div className={styles.mainDescription}>
-            <p>aksndhjla shdjashdl;ahjs;lkdj ksljdaklsjdkl;ajskldja skldjalksjdaskdljalsk;d asdhaksjdh jasd asdasdasd asdasd </p>
+            <p>Experiencia que resuelve, integridad que guía. Tu aliado legal en cada paso.</p>
             <Link className={styles.contactButton} href="/contact">CONTACT</Link>
           </div>
         </div>
@@ -20,7 +20,8 @@ export default function Home() {
         <section className={styles.biography}>
           <h1 className={styles.biographyTitle}>Biography</h1>
           <div className={styles.biographyDescription}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut sem sed leo eleifend tincidunt id ut lacus. Fusce feugiat ex risus, ac pharetra neque rutrum at. Donec gravida commodo ex ut cursus. Duis lacus libero, elementum in tincidunt eget, placerat non libero. Phasellus tortor elit, laoreet in vestibulum vitae, tristique in eros. Nullam vel diam tortor. Fusce vitae porttitor nisl. Duis lacinia porta ullamcorper. Aliquam erat volutpat. Curabitur a sollicitudin mi, non bibendum nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis molestie tellus. Nunc scelerisque elit ut lobortis ullamcorper. Vestibulum sagittis accumsan magna, a laoreet nisl mattis eget. Quisque dui ante, maximus vitae lacus a, volutpat iaculis nisl. Pellentesque a est eget diam consectetur consequat sed sed quam.</p>
+            <p>Javier Clot, a notarial lawyer, integrates modernity and tradition in his legal practice. He has developed a distinct method that combines his clients' modern needs with legal rigour through more than three decades of experience. His career has led him through a variety of experiences, which have broadened his perspective on the law. As an independent professional, Javier is committed to providing a close and accessible service, maintaining his sophistication and excellence. His commitment is evident: to provide cutting-edge and efficient legal solutions, with a personal touch that distinguishes from the first meeting.
+            </p>
           </div>
         </section>
         <hr className="commonHr" />
