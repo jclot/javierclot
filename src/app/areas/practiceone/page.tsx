8d9b2a27faf17bb887768b9f10e4ffbd5db033fd
JulianClotCordoba <jclot60815@ufide.ac.cr>
2024-08-19 14:53:44 -0600

    Create the base of the cards for the areas of practice pages

--- src/app/areas/practiceone/page.tsx
+++ src/app/areas/practiceone/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import styles from './practiceone.module.css';
+import styles from '../areas.module.css';
+import Image from "next/image";
 
 const PracticeOne: React.FC = () => {
   return (
@@ -10,6 +11,18 @@ const PracticeOne: React.FC = () => {
         </div>
         <div className={styles.areasDescription}>
           <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut sem sed leo eleifend tincidunt id ut lacus. Fusce feugiat ex risus, ac pharetra neque rutrum at. Donec gravida commodo ex ut cursus. Duis lacus libero, elementum in tincidunt eget, placerat non libero. Phasellus tortor elit, laoreet in vestibulum vitae, tristique in eros. Nullam vel diam tortor. Fusce vitae porttitor nisl. Duis lacinia porta ullamcorper. Aliquam erat volutpat. Curabitur a sollicitudin mi, non bibendum nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis molestie tellus. Nunc scelerisque elit ut lobortis ullamcorper. Vestibulum sagittis accumsan magna, a laoreet nisl mattis eget. Quisque dui ante, maximus vitae lacus a, volutpat iaculis nisl. Pellentesque a est eget diam consectetur consequat sed sed quam.</p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <Image
+          src="/fotoFirma.jpg"
+          width={1000}
+          height={1000}
+          alt="AreasOfPractice"
+        />
+        <div className={styles.cardContent}>
+          <h2 className={styles.cardTitle}>Practice 1</h2>
+          <p className={styles.cardDescription}>This is a description of the card It</p>
         </div>
       </div>
     </div>
