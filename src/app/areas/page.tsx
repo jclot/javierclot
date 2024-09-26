7a5da1b592db715a8e08aae76f2fc8453d190ccc
JulianClotCordoba <jclot60815@ufide.ac.cr>
2024-09-26 03:10:17 -0600

    Fix again some inconsistencies in the mobile version of the web site.

--- src/app/areas/page.tsx
+++ src/app/areas/page.tsx
@@ -10,7 +10,7 @@ const Areas: React.FC = () => {
           <h1>Areas of practice</h1>
         </div>
         <div className={styles.areasDescription}>
-          <p>Javier Clot guarantees full covering for his customers legal needs by providing a thorough approach in a variety of legal areas. His expertise in labor law helps him to mediate disputes between employers and employees, while his background in criminal law allows him to effectively defend people facing legal action. Furthermore, he counsels businesses and entrepreneurs in the field of commercial law about the safeguarding of their rights and the streamlining of their processes. Lastly, his expertise in condominium law guarantees appropriate administration and settlement of conflicts pertaining to horizontal real estate. To deliver efficient legal solutions, Javier Clot blends his experience with a tailored strategy in each practice area.</p>
+          <p>Javier Clot guarantees full covering for his customers legal needs by providing a thorough approach in a variety of legal areas. His expertise in labor law helps him to mediate disputes between employers and employees, while his background in criminal law allows him to effectively defend people facing legal action. Furthermore, he counsels businesses and entrepreneurs in the field of commercial law about the safeguarding of their rights and the streamlining of their processes. Lastly, his expertise in condominium law guarantees appropriate administration and settlement of conflicts pertaining to horizontal real estate. To deliver efficient legal solutions, Javier Clot blends his experience with a tailored strategy in each practice area, as the following sections.</p>
         </div>
       </div>
       <Cards />
